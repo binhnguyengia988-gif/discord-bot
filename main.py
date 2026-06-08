ecf7b27500a8f1f5edec391da6b8708ed850f77e
binhnguyengia988-gif <binhnguyengia988@gmail.com>
2026-06-08 15:19:26 +0700

    Initialize Discord bot with ping command

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot đã online: {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong! Bot đang chạy từ Render.')
+
+bot.run(os.environ['TOKEN'])
